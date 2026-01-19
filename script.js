50f9b42d9be3a11040754e947e71cdeb3f4538ba
Priyanshu Barnwal <pbarnwal275@gmail.com>
2026-01-19 18:01:09 +0530

    Generate 16x16 grid cells dynamically
    Create 256 cell divs using nested loops in JS and append to the container div

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const container = document.querySelector(".container");
+
+for (let j = 0; j < 16; j++) {
+  const div = document.createElement("div");
+  div.classList.add("cell");
+  container.appendChild(div);
+}
