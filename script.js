aac40f76ceddbb9c14a61124070a8dff311ad7a8
Priyanshu Barnwal <pbarnwal275@gmail.com>
2026-01-19 19:22:26 +0530

    Add hover interaction to grid cells
    Colour cells on mouse enter to create drawing effect

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const container = document.querySelector(".container");
 
-for (let j = 0; j < 16; j++) {
-  const div = document.createElement("div");
-  div.classList.add("cell");
-  container.appendChild(div);
+for (let i = 0; i < 16; i++) {
+  for (let j = 0; j < 16; j++) {
+    const div = document.createElement("div");
+    div.classList.add("cell");
+    container.appendChild(div);
+  }
+}
+
+const cells = document.querySelectorAll(".cell");
+
+for (let cell of cells) {
+  cell.addEventListener("mouseenter", () => {
+    cell.style.backgroundColor = "black";
+  });
 }
