561aa48e3a20dfa9e83ba7cda3481b514f410206
Priyanshu Barnwal <pbarnwal275@gmail.com>
2026-01-19 21:10:28 +0530

    Handle grid resize button click
    Prompt user for new grid size on button click

--- script.js
+++ script.js
@@ -8,18 +8,23 @@ const container = document.createElement("div");
 container.classList.add("container");
 body.appendChild(container);
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const div = document.createElement("div");
-    div.classList.add("cell");
-    container.appendChild(div);
-  }
-}
-
 const cells = document.querySelectorAll(".cell");
 
 for (let cell of cells) {
   cell.addEventListener("mouseenter", () => {
     cell.style.backgroundColor = "black";
   });
+}
+
+button.addEventListener("click", () => {
+  const size = prompt("Set the grid size upto 100:", "");
+  console.log(+size);
+});
+
+for (let i = 0; i < 16; i++) {
+  for (let j = 0; j < 16; j++) {
+    const div = document.createElement("div");
+    div.classList.add("cell");
+    container.appendChild(div);
+  }
 }
